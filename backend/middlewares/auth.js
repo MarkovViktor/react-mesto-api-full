16a01acf7983a1f0e74910d662f4ccebecd6a1ba
Viktor Markov <markov.viktor.an@yandex.ru>
2022-09-02 20:06:50 +0300

    bugfix11

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,15 +3,17 @@ const UnauthorizedError = require('../components/UnauthorizedError');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-module.exports.isAuth = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) {
-    throw new UnauthorizedError('Необходима авторизация');
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
+
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
